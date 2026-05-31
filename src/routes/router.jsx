c605a35c864539c6270d698c11a9b59ed8c650e4
Ramisamimhaz <tasnimanisa89@gmail.com>
2026-05-31 17:40:45 +0600

    added src file in this project

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layouts/MainLaoyt/MainLayout";
+import MainLayout from "../layouts/MainLayout/MainLayout.jsx";
 import DashboardLayout from "../layouts/DashboardLayout/DashboardLayout";
 import MyOrders from "../pages/Dashboard/MyOrders/MyOrders";
 import Login from "../auth/Login/Login";
